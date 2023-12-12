bc02429dc4a21ed75568d431e9ab403d0f3611e2
Felipe <felipesantosdd@gmail.com>
2023-12-12 14:29:57 -0300

    fix: corerecao no deploy

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,6 @@ import { AppDataSource } from "./data-source"
 
 AppDataSource.initialize()
     .then(() => {
-        console.log(process.env)
         const PORT = parseInt(process.env.API_PORT) || 3000
         app.listen(PORT, () => console.log(`Tudo pronto, servidor rodando na porta ${PORT}`))
     })
